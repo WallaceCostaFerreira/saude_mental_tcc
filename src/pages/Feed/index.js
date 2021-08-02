3372823c42dc3b24c4ccdc8c61b69c24230ef13d
Wallace Costa Ferreira <80697255+WallaceCostaFerreira@users.noreply.github.com>
2021-08-02 12:42:19 -0300

    Inicio da tela de feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from 'react';
-import { KeyboardAvoidingView, Text, TouchableOpacity } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import { View, Text, TouchableOpacity, StatusBar, Image, TextInput } from 'react-native';
 
 import styles from "./style"
 import firebase from '../../config/Firebaseconfig';
@@ -8,18 +9,67 @@ export default function Feed({ navigation }){
 
     const logOut = () =>{
         firebase.auth().signOut().then(() => {
-            navigation.navigate("SplashScreen");
+            navigation.navigate("Login");
         }).catch((error) => {
             // An error happened.
         });
     }
 
     return(
-        <KeyboardAvoidingView style={styles.container}>
-            <Text>Saúde Mental - Feed</Text>
-            <TouchableOpacity onPress={logOut}>
+        <View style={styles.container}>
+            <StatusBar
+                animated={true}
+                backgroundColor="#98FB98"
+                hidden={false} />
+            <View style={styles.row}>
+                <View style={styles.profileView}>
+                    <View style={styles.tinyImg}>
+                        <Feather
+                            name={'user'}
+                            color={'#000000'}
+                        />
+                    </View>
+                    <Text style={styles.txtNameProfile}>Wallace Costa</Text>
+                </View>
+                <View style={styles.publishRow}>
+                    <TouchableOpacity
+                        style={styles.publishButton}>
+                        <Feather
+                            name={'plus'}
+                            size={18}
+                            color={'#fff'}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.publishButton}>
+                        <Feather
+                            name={'settings'}
+                            size={18}
+                            color={'#fff'}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.publishButton}
+                        onPress={logOut}>
+                        <Feather
+                            name={'log-out'}
+                            size={18}
+                            color={'#fff'}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            {/* <Text>Saúde Mental - Feed</Text>
+            <TextInput
+                style={styles.publishInput}
+                type="text"
+                placeholder="Publish here"
+            />
+            <TouchableOpacity 
+                style={styles.logoutButton}
+                onPress={logOut}>
                 <Text>Deslogar</Text>
-            </TouchableOpacity>
-        </KeyboardAvoidingView>
+            </TouchableOpacity> */}
+        </View>
     )
 }
--- src/pages/Feed/style.js
+++ src/pages/Feed/style.js
@@ -4,8 +4,63 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems: 'center',
-        justifyContent: 'center',
         backgroundColor: '#FFFFFF'
+    },logoutButton:{
+        width:100,
+        height:30,
+        backgroundColor: '#FFFF00',
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:10,
+        borderRadius:20,
+        borderColor:'#FF9933',
+        borderWidth:1,
+    },row:{
+        width:'100%',
+        flexDirection:'row',
+        padding:4,
+        marginBottom:10,
+        justifyContent:'space-between',
+        alignItems:'center',
+        borderBottomWidth:1,
+        borderBottomColor:'#F5F5F5',
+    },profileView:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'flex-end'
+    },publishRow:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-around'
+    },tinyImg:{
+        width:26,
+        height:26,
+        backgroundColor:'#98FB98',
+        borderRadius:13,
+        marginRight:10,
+        justifyContent:'center',
+        alignItems:'center',
+        borderWidth:1,
+    },publishInput:{
+        borderWidth:1,
+        fontSize:10,
+        height:26,
+        width:130,
+        paddingLeft:10,
+        borderTopLeftRadius:13,
+        borderBottomLeftRadius:13,
+        borderColor:'#98FB98',
+    },publishButton:{
+        width:30,
+        height:30,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:15,
+        backgroundColor:'#98FB98',
+        marginLeft:8,
+    },txtNameProfile:{
+        fontSize:14,
+        color:'#C0C0C0'
     }
 });
 
