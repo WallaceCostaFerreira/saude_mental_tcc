720964f5680691616871ae92bcea74189cde32b8
Wallace Costa Ferreira <80697255+WallaceCostaFerreira@users.noreply.github.com>
2021-08-13 17:12:09 -0300

    Criação da tela de publicação.

---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export default theme = {
+    colors:{
+        primary:"#98FB98",
+        blue:"#98C3FB",
+        textGray: "rgba(0,0,0,0.5)",
+        white: "#fff",
+        lightGray: "rgba(0,0,0,0.05)",
+    }
+};
